e2197d3bb951cbc8e3829d7e3ffc9f9890f92d59
Shane Oh <this.shaneoh@gmail.com>
2026-07-29 14:02:58 +0900

    fix: harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -16,6 +16,6 @@ require('which-key').add { { '<leader>n', group = 'Harpoo[n]' } }
 vim.keymap.set('n', '<leader>na', function() harpoon:list():add() end, { desc = 'Harpoo[N] [A]dd file' })
 vim.keymap.set('n', '<leader>nl', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoo[N] [L]ist files' })
 
-vim.keymap.set('n', '<leader>nh', function() harpoon.ui:toggle_quick_menu(harpoon:list():select(1)) end, { desc = 'Harpoo[N] [H] (First)' })
-vim.keymap.set('n', '<leader>nj', function() harpoon.ui:toggle_quick_menu(harpoon:list():select(2)) end, { desc = 'Harpoo[N] [J] (Second)' })
-vim.keymap.set('n', '<leader>nk', function() harpoon.ui:toggle_quick_menu(harpoon:list():select(3)) end, { desc = 'Harpoo[N] [K] (Third)' })
+vim.keymap.set('n', '<leader>nh', function() harpoon:list():select(1) end, { desc = 'Harpoo[N] [H] (First)' })
+vim.keymap.set('n', '<leader>nj', function() harpoon:list():select(2) end, { desc = 'Harpoo[N] [J] (Second)' })
+vim.keymap.set('n', '<leader>nk', function() harpoon:list():select(3) end, { desc = 'Harpoo[N] [K] (Third)' })
